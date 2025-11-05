f2a502c056de9c0215ef96fe1f7510297567a565
Hilasmic <163965137+ROSHANDAPPU@users.noreply.github.com>
2025-11-05 12:38:14 -0600

    Configure GitHub Pages deployment with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/backup.LBM1/' : '/',
+  build: {
+    outDir: 'dist'
+  },
   publicDir: 'Public',
   server: {
     host: "::",
